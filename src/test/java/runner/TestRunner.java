4b37a78b2896fc6e855919f7dde3ca26152ec93b
Léon Mofor <leon.mofor@groupebia.com>
2019-03-07 16:46:18 +0100

    maven cucumber report config

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features = {"src/test/java/Features"},glue = "Steps")
+@CucumberOptions(features = {"src/test/java/Features"}, format = {"json:target/cucumber.json","html:target/site/cucumber-pretty"},glue = "Steps")
 
 public class TestRunner {
 }
